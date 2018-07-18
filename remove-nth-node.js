d6695e750671f0026b0b3cabf6ef8d2f8589141d
Kevin <kevintun1122@gmail.com>
2018-07-18 16:24:39 -0700

    - remove nth node from end

---
+++ remove-nth-node.js
@@ -0,0 +1,21 @@
+let removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy, second = dummy;
+  for (let i = 0; i < n + 1 ; i++) {
+    first = first.next;
+  }
+  while(first) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+};
+
+let node = new ListNode(1);
+let a = node.next = new ListNode(2);
+let b = a.next = new ListNode(3);
+let c = b.next = new ListNode(4);
+
+removeNthFromEnd(node, 4);
